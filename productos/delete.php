ca779e9dc7cdd9ed1aac89d7f7f66cbee334b096
luisescobar09 <1719110043@utectulancingo.edu.mx>
2020-11-23 00:08:47 +0000

    eliminar producto funcionando

--- productos/delete.php
+++ productos/delete.php
@@ -9,17 +9,17 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-    <title>Actualizar producto</title>
+    <title>Eliminar producto</title>
 </head>
 
 <body>
     <div class="container-fluid">
-        <h1 class="text-center text-danger mt-2 pt-2">Actualizar producto:</h1> <br>
+        <h1 class="text-center text-danger mt-2 pt-2">Eliminar producto:</h1>  <br>
         <a href="index.php">Regresar a lista</a>
 
-        <form action='db/db_update.php' method='GET'> <br>
-            <?php include 'db/details_update.php'; ?> <br>
-            <button type='submit' class="btn btn-success btn-lg my-1 float-right">Confirmar actualización</button>
+        <form action='db/db_delete.php' method='GET'> <br>
+            <?php include 'db/db_view.php'; ?> <br>
+            <button type='submit' class='btn btn-danger btn-lg my-1 float-right'>Eliminar</button>
         </form>
 
     </div>
